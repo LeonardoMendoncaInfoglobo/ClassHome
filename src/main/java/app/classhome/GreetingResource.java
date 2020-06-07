c6993531d4705425a8ce7a7dfa19502c0e5abf84
Leonardo Mendonca <leonardo.mendonca@infoglobo.com.br>
2020-06-06 21:25:08 -0300

    Criando mais um endpoint para teste

--- src/main/java/app/classhome/GreetingResource.java
+++ src/main/java/app/classhome/GreetingResource.java
@@ -2,6 +2,7 @@ package app.classhome;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,4 +14,12 @@ public class GreetingResource {
     public String hello() {
         return "hello";
     }
+
+    @GET
+    @Path("/soma/{n1}/{n2}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String soma(@PathParam("n1") int n1, @PathParam("n2") int n2) {
+        return "A soma é: " + (n1 + n2);
+    }
+
 }
